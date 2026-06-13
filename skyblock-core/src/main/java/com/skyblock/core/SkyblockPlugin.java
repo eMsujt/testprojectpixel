905826258712c443073d064c561546022e2da2f1
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-13 19:14:33 +0200

    Add skyblock-core/src/main/java/com/skyblock/core/SkyblockPlugin.java extending JavaPlugin: onEnable instantiates all 15 managers (BankManag (#697)
    By Jarvis agent Oracle.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

---
+++ skyblock-core/src/main/java/com/skyblock/core/SkyblockPlugin.java
@@ -0,0 +1,40 @@
+package com.skyblock.core;
+
+import com.skyblock.core.accessory.AccessoryManager;
+import com.skyblock.core.auction.AuctionManager;
+import com.skyblock.core.bank.BankManager;
+import com.skyblock.core.bazaar.BazaarManager;
+import com.skyblock.core.dungeon.DungeonManager;
+import com.skyblock.core.enchanting.EnchantingManager;
+import com.skyblock.core.fishing.FishingManager;
+import com.skyblock.core.garden.GardenManager;
+import com.skyblock.core.hotm.HOTMManager;
+import com.skyblock.core.island.IslandManager;
+import com.skyblock.core.mayor.MayorManager;
+import com.skyblock.core.pets.PetsManager;
+import com.skyblock.core.profile.ProfileManager;
+import com.skyblock.core.reforge.ReforgeManager;
+import com.skyblock.core.slayer.SlayerManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyblockPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        BankManager.getInstance();
+        MayorManager.getInstance();
+        AuctionManager.getInstance();
+        BazaarManager.getInstance();
+        DungeonManager.getInstance();
+        IslandManager.getInstance();
+        GardenManager.getInstance();
+        SlayerManager.getInstance();
+        FishingManager.getInstance();
+        HOTMManager.getInstance();
+        PetsManager.getInstance();
+        EnchantingManager.getInstance();
+        ReforgeManager.getInstance();
+        AccessoryManager.getInstance();
+        ProfileManager.getInstance();
+    }
+}
