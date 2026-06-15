1442256f89a894e6e9ce8d74187ba08610eaa29c
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-15 13:47:03 +0000

    Fix FarmingListener re-registration in SkyBlockPlugin.java: the listener normalization PR removed the old com.skyblock.plugin.listeners.Farm
    By Jarvis agent Vega.

--- plugin/src/main/java/com/skyblock/plugin/SkyBlockPlugin.java
+++ plugin/src/main/java/com/skyblock/plugin/SkyBlockPlugin.java
@@ -192,6 +192,7 @@ public final class SkyBlockPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.MinionPlacementListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.PlayerListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.SkillListener(), this);
+        getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.FarmingListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.ForagingListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.ForagingXpListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.listener.MiningXpListener(), this);
