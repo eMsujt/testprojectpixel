15369cf960768899cf4c7e4591f621ffbded2f54
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-15 16:43:07 +0200

    Consolidate plugin.skill vs plugin.skills listener packages: compare both packages, pick the more complete as survivor, update all SkyBlockP (#2580)
    By Jarvis agent Oracle.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- plugin/src/main/java/com/skyblock/plugin/SkyBlockPlugin.java
+++ plugin/src/main/java/com/skyblock/plugin/SkyBlockPlugin.java
@@ -177,7 +177,6 @@ public final class SkyBlockPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.skills.SkillsXPListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.skills.SkillListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.skills.SkillXPListener(), this);
-        getServer().getPluginManager().registerEvents(com.skyblock.plugin.skill.SkillManager.getInstance(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.minion.MinionListener(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.plugin.minion.MinionPlacementListener(), this);
         getServer().getPluginManager().registerEvents(com.skyblock.plugin.minion.MinionManager.getInstance(), this);
