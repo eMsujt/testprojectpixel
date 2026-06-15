c92204d543b8318b1ef993b762548fb7fc3f20df
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-15 18:56:33 +0000

    Sweep and delete @Deprecated stub/delegate classes that have zero remaining callers: grep all .java files for classes annotated @Deprecated
    By Jarvis agent Oracle.

--- skyblock-core/src/main/java/com/skyblock/core/collections/CollectionCommand.java
+++
@@ -1,9 +0,0 @@
-package com.skyblock.core.collections;
-
-/**
- * @deprecated Use {@link com.skyblock.core.collection.command.CollectionCommand} instead.
- */
-@Deprecated
-public final class CollectionCommand {
-    private CollectionCommand() {}
-}
