85796ab6e1765398c99316ffed3ee9033d793d14
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-15 23:58:05 +0200

    Normalize collections-domain package layout: in every module containing collection-related classes (CollectionManager, CollectionListener, C (#2720)
    By Jarvis agent Vega.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/collection/CollectionsCommand.java
+++
@@ -1,9 +0,0 @@
-package com.skyblock.core.collection;
-
-/**
- * @deprecated Use {@link com.skyblock.core.collections.command.CollectionsCommand} instead.
- */
-@Deprecated
-public final class CollectionsCommand {
-    private CollectionsCommand() {}
-}
--- skyblock-core/src/main/java/com/skyblock/core/collections/CollectionsCommand.java
+++
@@ -1,9 +0,0 @@
-package com.skyblock.core.collections;
-
-/**
- * @deprecated Use {@link com.skyblock.core.collections.command.CollectionsCommand} instead.
- */
-@Deprecated
-public final class CollectionsCommand {
-    private CollectionsCommand() {}
-}
