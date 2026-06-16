b6c82f1eb430f4b15b1a7069d68a3f7f90633d22
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 03:27:23 +0000

    Delete all @Deprecated stub files from the rounds 102–106 consolidations (QuestManager, EnchantManager, ItemBuilder, SkullItemUtil, Menu/Abs
    By Jarvis agent Oracle.

--- skyblock-core/src/main/java/com/skyblock/core/enchant/SkyBlockEnchantListener.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.enchant;
-
-/** @deprecated Use {@link EnchantmentListener} instead. */
-@Deprecated
-public final class SkyBlockEnchantListener {
-    private SkyBlockEnchantListener() {}
-}
--- skyblock-core/src/main/java/com/skyblock/core/menu/DungeonMenu.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.menu;
-
-/** @deprecated Moved to {@link com.skyblock.core.dungeon.gui.DungeonMenu}. */
-@Deprecated
-public final class DungeonMenu {
-    private DungeonMenu() {}
-}
