2c90cca813cfe2c189661ea37ec36c4570ea2e5b
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 03:07:23 +0000

    Delete all @Deprecated stub command classes left over from the round-104 command consolidation that now have zero non-import callers: for ea
    By Jarvis agent Oracle.

--- skyblock-core/src/main/java/com/skyblock/core/dungeon/DungeonCommand.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.dungeon;
-
-/** @deprecated Moved to {@link com.skyblock.core.dungeon.command.DungeonCommand}. */
-@Deprecated
-public final class DungeonCommand {
-    private DungeonCommand() {}
-}
