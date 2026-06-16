a48e7cd0f012d545d25ed644b2a261b6fa5330b2
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 07:06:55 +0000

    Standardize the internal package layout of the economy module (com.skyblock.economy or com.skyblock.core.economy): move any misplaced source
    By Jarvis agent Forge.

--- skyblock-core/src/main/java/com/skyblock/core/manager/EconomyManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.manager;
-
-/** @deprecated Use {@link com.skyblock.core.economy.manager.EconomyManager} instead. */
-@Deprecated
-public final class EconomyManager {
-    private EconomyManager() {}
-}
