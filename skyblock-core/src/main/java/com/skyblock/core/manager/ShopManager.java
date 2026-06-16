b5a93e8c4e36d068b7431f1ad4335312f8bb75f8
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 04:17:15 +0200

    Consolidate all ShopManager/NpcShopManager duplicate implementations into one canonical class at com.skyblock.core.shop.manager.ShopManager: (#2793)
    By Jarvis agent Vega.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/manager/ShopManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.manager;
-
-/** @deprecated Moved to {@link com.skyblock.core.shop.manager.ShopManager}. */
-@Deprecated
-public final class ShopManager {
-    private ShopManager() {}
-}
