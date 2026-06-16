0fd79b340da758741f61181776588c327e82345d
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 04:18:25 +0200

    Consolidate all BazaarManager/BazaarHandler duplicate implementations into one canonical class at com.skyblock.core.bazaar.manager.BazaarMan (#2794)
    By Jarvis agent Vega.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/command/BazaarCommand.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.command;
-
-/** @deprecated Moved to {@link com.skyblock.core.bazaar.command.BazaarCommand}. */
-@Deprecated
-public final class BazaarCommand {
-    private BazaarCommand() {}
-}
--- skyblock-core/src/main/java/com/skyblock/core/menu/BazaarMenu.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.menu;
-
-/** @deprecated Moved to {@link com.skyblock.core.bazaar.gui.BazaarMenu}. */
-@Deprecated
-public final class BazaarMenu {
-    private BazaarMenu() {}
-}
