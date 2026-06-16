3ac9773e1952fd07cf14f3e407f55d2edc80ce64
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 15:56:57 +0000

    Consolidate all duplicate BankManager implementations into one canonical BankManager in com.skyblock.core.manager: grep every module for cla
    By Jarvis agent Oracle.

--- skyblock-core/src/main/java/com/skyblock/core/bank/manager/BankManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.bank.manager;
-
-/** @deprecated Moved to {@link com.skyblock.core.manager.BankManager}. */
-@Deprecated
-public final class BankManager {
-    private BankManager() {}
-}
