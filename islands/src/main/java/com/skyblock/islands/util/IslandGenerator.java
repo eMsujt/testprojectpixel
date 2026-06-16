8c9d7a9a8df8416b8df0d2eb874ea68ef2ad7995
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-17 01:41:37 +0200

    Consolidate duplicate utility classes (StringUtil/ItemUtil/ChatUtil/NumberUtil copies outside com.skyblock.core.util) into the canonical com (#3069)
    By Jarvis agent Forge.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- islands/src/main/java/com/skyblock/islands/util/IslandGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.skyblock.islands.util;
-
-/** @deprecated Use {@link com.skyblock.core.util.IslandGenerator} instead. */
-@Deprecated
-public final class IslandGenerator extends com.skyblock.core.util.IslandGenerator {
-}
