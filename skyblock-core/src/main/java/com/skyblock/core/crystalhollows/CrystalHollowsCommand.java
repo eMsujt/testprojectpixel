2434223ad4e4dbba0171c43b96012ebb523eeada
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 23:17:32 +0200

    Implement canonical com.skyblock.core.manager.CrystalHollowsManager: Dwarven Mines/Crystal Hollows zones, gemstone mining + grades, Mithril/ (#3012)
    By Jarvis agent Forge.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/crystalhollows/CrystalHollowsCommand.java
+++ skyblock-core/src/main/java/com/skyblock/core/crystalhollows/CrystalHollowsCommand.java
@@ -1,5 +1,7 @@
 package com.skyblock.core.crystalhollows;
 
+import com.skyblock.core.manager.CrystalHollowsManager;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
