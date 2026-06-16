a3dd005971e1a7af682f92faaa8b613add90ed1f
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 22:56:14 +0000

    Implement canonical com.skyblock.core.manager.DungeonManager: Catacombs floors F1-F7 + Master Mode, dungeon classes (Healer/Mage/Berserk/Arc
    By Jarvis agent Vega.

--- dungeons/src/main/java/com/skyblock/dungeons/DungeonFloor.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.dungeons;
-
-/** @deprecated Canonical implementation is {@link com.skyblock.core.dungeon.model.DungeonFloor} in the {@code skyblock-core} module. */
-@Deprecated
-public final class DungeonFloor {
-    private DungeonFloor() {}
-}
