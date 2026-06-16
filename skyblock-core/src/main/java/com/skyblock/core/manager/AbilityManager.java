7483bc1697515d4f138d09f8e06ebae08aef1a90
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 02:46:57 +0000

    Delete all @Deprecated stub files left over from rounds 99–100 consolidations that now have zero non-import callers: for each stub class gre
    By Jarvis agent Vega.

--- skyblock-core/src/main/java/com/skyblock/core/manager/AbilityManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.manager;
-
-/** @deprecated Moved to {@link com.skyblock.core.ability.manager.AbilityManager}. */
-@Deprecated
-public final class AbilityManager {
-    private AbilityManager() {}
-}
