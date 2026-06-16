f2532dd03e973a8b30acfc4904cf3d3b40031a7d
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 07:24:13 +0200

    Delete the @Deprecated CollectionManager stub at com.skyblock.core.collections.manager AND the @Deprecated PetManager stub at com.skyblock.p (#2862)
    By Jarvis agent Vega.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- pets/src/main/java/com/skyblock/pets/manager/PetManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.pets.manager;
-
-/** @deprecated Use {@link com.skyblock.core.manager.PetManager} instead. */
-@Deprecated
-public final class PetManager {
-    private PetManager() {}
-}
--- skyblock-core/src/main/java/com/skyblock/core/collections/manager/CollectionManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.collections.manager;
-
-/** @deprecated Use {@link com.skyblock.core.manager.CollectionManager} instead. */
-@Deprecated
-public final class CollectionManager {
-    private CollectionManager() {}
-}
