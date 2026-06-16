fd7dd847667ad71b7b2147c148f6d20d0e9e6294
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 05:12:18 +0000

    Delete the @Deprecated BazaarManager forwarding stub left at com.skyblock.core.bazaar.manager from the round-118 consolidation: confirm zero
    By Jarvis agent Forge.

--- skyblock-core/src/main/java/com/skyblock/core/bazaar/manager/BazaarManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.bazaar.manager;
-
-/** @deprecated Use {@link com.skyblock.core.manager.BazaarManager} instead. */
-@Deprecated
-public final class BazaarManager {
-    private BazaarManager() {}
-}
