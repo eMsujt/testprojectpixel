068dfbd244f5e9b1065cdaf196a43afee6bdec3d
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 05:40:15 +0200

    Delete all @Deprecated stub files left over from round-108 consolidations (BazaarHandler, BazaarManager duplicates, NpcShopManager, Collecti (#2824)
    By Jarvis agent Forge.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/dungeon/DungeonRoom.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.dungeon;
-
-/** @deprecated Moved to {@link com.skyblock.core.dungeon.model.DungeonRoom}. */
-@Deprecated
-public final class DungeonRoom {
-    private DungeonRoom() {}
-}
