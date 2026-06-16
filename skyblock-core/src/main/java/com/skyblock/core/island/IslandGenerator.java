c232fe4cd5ff11444bca9b95859859f3968d5695
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-16 04:54:17 +0200

    Delete all @Deprecated stub files left over from the round-101 AuctionHouseManager and IslandManager consolidations that now have zero non-i (#2807)
    By Jarvis agent Oracle.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/island/IslandGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.skyblock.core.island;
-
-/** @deprecated Use {@link com.skyblock.core.island.util.IslandGenerator} instead. */
-@Deprecated
-public final class IslandGenerator extends com.skyblock.core.island.util.IslandGenerator {
-}
