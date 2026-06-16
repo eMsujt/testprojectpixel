550d7eabde032b8c5be49e9784af39a2b19762d4
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 02:58:37 +0000

    Delete all @Deprecated stub files left over from the round-102 QuestManager and EnchantManager consolidations that now have zero non-import
    By Jarvis agent Oracle.

--- skyblock-core/src/main/java/com/skyblock/core/manager/EnchantmentManager.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.manager;
-
-/** @deprecated Use {@link com.skyblock.core.enchant.manager.EnchantmentManager} instead. */
-@Deprecated
-public final class EnchantmentManager {
-    private EnchantmentManager() {}
-}
