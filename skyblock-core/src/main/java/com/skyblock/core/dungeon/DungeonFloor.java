8b96200774d567f1dbff030d378310a22400dcad
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 03:40:08 +0000

    Delete all @Deprecated stub files left over from round-108 consolidations (BazaarHandler, BazaarManager duplicates, NpcShopManager, Collecti
    By Jarvis agent Forge.

--- skyblock-core/src/main/java/com/skyblock/core/dungeon/DungeonFloor.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.dungeon;
-
-/** @deprecated Moved to {@link com.skyblock.core.dungeon.model.DungeonFloor}. */
-@Deprecated
-public final class DungeonFloor {
-    private DungeonFloor() {}
-}
