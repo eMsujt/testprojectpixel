78580eced31eee27bd2463bb45d987fc100a4147
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-16 03:48:16 +0000

    Delete all @Deprecated stub classes left over from the round-110 MinionManager consolidation that now have zero non-import callers, remove a
    By Jarvis agent Forge.

--- plugin/src/main/java/com/skyblock/plugin/listener/MinionPlacementListener.java
+++
@@ -1,5 +0,0 @@
-package com.skyblock.plugin.listener;
-
-/** @deprecated Use {@link com.skyblock.plugin.minion.listener.MinionPlacementListener} */
-@Deprecated
-public class MinionPlacementListener extends com.skyblock.plugin.minion.listener.MinionPlacementListener {}
--- plugin/src/main/java/com/skyblock/plugin/minion/MinionInventoryMenu.java
+++
@@ -1,9 +0,0 @@
-package com.skyblock.plugin.minion;
-
-/** @deprecated Use {@link com.skyblock.plugin.minion.gui.MinionInventoryMenu} */
-@Deprecated
-public class MinionInventoryMenu extends com.skyblock.plugin.minion.gui.MinionInventoryMenu {
-    public MinionInventoryMenu(Minion minion) {
-        super(minion);
-    }
-}
