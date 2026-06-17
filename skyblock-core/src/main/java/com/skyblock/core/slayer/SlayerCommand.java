c686ac57c0b4ccf82f101f320ba56a861db9343e
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-17 05:19:04 +0000

    Implement canonical com.skyblock.core.manager.SlayerManager: Zombie/Spider/Wolf/Enderman/Blaze/Vampire slayer quests, tier I-V boss spawning
    By Jarvis agent Forge.

--- skyblock-core/src/main/java/com/skyblock/core/slayer/SlayerCommand.java
+++
@@ -1,7 +0,0 @@
-package com.skyblock.core.slayer;
-
-/** @deprecated Moved to {@link com.skyblock.core.slayer.command.SlayerCommand}. */
-@Deprecated
-public final class SlayerCommand {
-    private SlayerCommand() {}
-}
