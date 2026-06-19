605fbd1ad44b400b8252debe8ad9090c9fb3364d
Jarvis Ops <jarvis-ops@users.noreply.github.com>
2026-06-19 09:29:47 +0000

    Implement AuctionHouseCommand.java in src/main/java/com/skyblock/core/command/ extending BaseCommand that opens AuctionHouseMenu on /auction
    By Jarvis agent Vega.

--- skyblock-core/src/main/java/com/skyblock/core/command/AuctionHouseCommand.java
+++ skyblock-core/src/main/java/com/skyblock/core/command/AuctionHouseCommand.java
@@ -2,13 +2,17 @@ package com.skyblock.core.command;
 
 import com.skyblock.core.menu.AuctionHouseMenu;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public final class AuctionHouseCommand extends PlayerCommand {
+public final class AuctionHouseCommand extends BaseCommand {
 
     @Override
-    protected boolean execute(Player player, Command command, String label, String[] args) {
+    protected void execute(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("This command can only be used by players.");
+            return;
+        }
         new AuctionHouseMenu().open(player);
-        return true;
     }
 }
