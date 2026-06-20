7efb132eae4b3c67ea4dbaef4ee695cc12bbad01
Dion <138459453+eMsujt@users.noreply.github.com>
2026-06-20 05:28:16 +0200

    Consolidate leftover thin listener stubs: scan src/main/java/com/skyblock/core/listener/ for any files under 60 lines with a single @EventHa (#3688)
    By Jarvis agent Vega.
    
    Co-authored-by: Jarvis Ops <jarvis-ops@users.noreply.github.com>

--- skyblock-core/src/main/java/com/skyblock/core/SkyBlockCore.java
+++ skyblock-core/src/main/java/com/skyblock/core/SkyBlockCore.java
@@ -642,9 +642,7 @@ public final class SkyBlockCore extends JavaPlugin {
         getServer().getPluginManager().registerEvents(com.skyblock.core.listener.MiningListener.getInstance(), this);
         getServer().getPluginManager().registerEvents(com.skyblock.core.listener.FarmingListener.getInstance(), this);
         getServer().getPluginManager().registerEvents(com.skyblock.core.listener.ChatListener.getInstance(), this);
-        getServer().getPluginManager().registerEvents(com.skyblock.core.listener.SkyBlockChatListener.getInstance(), this);
-        getServer().getPluginManager().registerEvents(com.skyblock.core.listener.DeathListener.getInstance(), this);
-        getServer().getPluginManager().registerEvents(com.skyblock.core.listener.InventoryListener.getInstance(), this);
+        getServer().getPluginManager().registerEvents(com.skyblock.core.listener.PlayerEventListener.getInstance(), this);
         getServer().getPluginManager().registerEvents(new com.skyblock.core.mob.MobLootListener(com.skyblock.core.mob.MobLootManager.getInstance()), this);
         ChatCommand chatCommand = new ChatCommand(chatManager);
         getCommand("chat").setExecutor(chatCommand);
